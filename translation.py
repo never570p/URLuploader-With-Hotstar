a7f56034d1ac7fe32f15f4d76e25eda7ddf98651
BRYLLEDUCATION <87828319+BRYLLEDUCATION@users.noreply.github.com>
2021-07-23 22:59:55 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -87,8 +87,6 @@ URL | newfilename | username | password"""
     
     SHOW_THUMB = "Use /deletethumbnail to clear this thumbnail."
     
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @hx_urluploadbot"
-    
     DONATE_TEXT = "<b>If you like our bot and want to donate to an educational institution of India <big>BRYLL EDUCATION</big>, then kindly contact our chatbot👉 @bryll_helpdesk_bot.</b>"
     
     JOIN_TEXT = """<b>Join the channels of <big>BRYLL EDUCATION</big>👇👇👇</b>
