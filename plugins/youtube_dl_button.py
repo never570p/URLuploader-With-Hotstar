1e5caebd68191b1ddfed8bd998961a67846338e2
BRYLLEDUCATION <87828319+BRYLLEDUCATION@users.noreply.github.com>
2021-08-28 13:13:10 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -360,5 +360,4 @@ async def youtube_dl_call_back(bot, update):
                 text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS.format(time_taken_for_download, time_taken_for_upload),
                 chat_id=update.message.chat.id,
                 message_id=update.message.message_id,
-                disable_web_page_preview=True
-            )
+             
